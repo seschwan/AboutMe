9a01d2452425148dda02932c06004eb6cadd402a
Corey Sessions <corey.iosdev@yahoo.com>
2019-04-25 20:09:37 -0600

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by Corey Sessions on 4/25/19.
+//  Copyright © 2019 Corey Sessions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
